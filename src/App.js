5629b55e40ca4d494a7a88a722b85e3ecea7b026
Thili007 <tpunchihewage@gmail.com>
2022-10-16 19:42:23 +0200

    added files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import "./App.css";
 
 export default function App() {
-    return (
-        <div>
-            <h1>My React App</h1>
-        </div>
-    )
+  return (
+    <div className="main-container">
+      <div className="stamp-Name">
+        <p className="text-stamp">STAMP</p>
+      </div>
+      <div className="sender-div">
+        <p>Mr. Sender</p>
+        <p>123 Fake St.</p>
+        <p>Boston,MA 02118</p>
+      </div>
+      <div className="Receiver-div">
+        <p>Mrs. Reciver</p>
+        <p>123 Fake St.</p>
+        <p>San Francisco, CA 94101</p>
+      </div>
+    </div>
+  );
 }
